74a1fe7fa183f721f14ae195964b4449b52e9e07
github-user865436 <132678398+github-user865436@users.noreply.github.com>
2025-04-08 15:53:21 -0500

    Create DefaultData.lua

---
+++ DefaultData.lua
@@ -0,0 +1,14 @@
+return {
+		RankedGames = {
+			{
+				Rating = 1500,
+				RatingDeviation = 200,
+				Volatility = 0.06
+			},
+		},
+		PairingSettings = {
+			Matchmaking = false,
+			DivisionDiffHigh = 1,
+			ClientLatencyHigh = 180,
+		},
+	}
