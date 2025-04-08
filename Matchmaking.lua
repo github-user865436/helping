193c8071e94e492593290a8a208196dbfc1031df
github-user865436 <132678398+github-user865436@users.noreply.github.com>
2025-04-08 08:44:43 -0500

    Update Matchmaking.lua

--- Matchmaking.lua
+++ Matchmaking.lua
@@ -1,8 +1,19 @@
-local datastoreservice = game:GetService("DataStoreService"):GetGlobalDataStore("RatingsContribution")
+local globalDS = game:GetService("DataStoreService"):GetGlobalDataStore("RatingsContribution")
+local replicatedstorage = game:GetService("ReplicatedStorage")
 
 local players = {}
 
+local events = replicatedstorage.Events
+local assets = replicatedstorage.Assets
+
+local rankings = require(assets.Ranks)
+local data = globalDS:GetAsync(id).
+
+events.StartPairing.OnServerEvent:Connect(function(player)
+	
+end)
+
 game.Players.PlayerAdded:Connect(function(player)
 	local id = player.UserId
-	players[id] = datastoreservice:GetAsync(id).PairingSettings
+	players[id] = globalDS:GetAsync(id).PairingSettings
 end)
