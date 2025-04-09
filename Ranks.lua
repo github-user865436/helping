010ff3bd2e5aabe817d15e5548a9074afb09939d
github-user865436 <132678398+github-user865436@users.noreply.github.com>
2025-04-09 07:52:32 -0500

    Create Ranks.lua

---
+++ Ranks.lua
@@ -0,0 +1,21 @@
+return {
+	"Bronze1",
+	"Bronze2",
+	"Bronze3",
+	"Silver1",
+	"Silver2",
+	"Silver3",
+	"Gold1",
+	"Gold2",
+	"Gold3",
+	"Platinum1",
+	"Platinum2",
+	"Platinum3",
+	"Diamond1",
+	"Diamond2",
+	"Diamond3",
+	"Onyx1",
+	"Onyx2",
+	"Onyx3",
+	"Nemesis"
+}
